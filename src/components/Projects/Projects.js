925c1cf19e414575c7de79eb5555d1a8e3cbfb6f
FelipeCosta <felipesaev@gmail.com>
2020-06-16 19:53:08 -0300

    add display grid and adjust values to responsividade

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,35 +7,43 @@ const Projects = () => {
     <div className="container">
       <div className="container-text">
         <div className="context-title">
-          <h2>Meus Projetos</h2>
-          <h3>Uma das melhores maneiras de apender é colocando em prática,
-          saindo da zona de conforto.
+          <h2 className="title">Minhas habilidades</h2>
+          <h3 className="description">As minha habilidades são colocadas em práticas todos os dias,
+            não acredito em um nivel perfeito e sim em um nivel de mentalidade que sempre há algo a aprender
           </h3>
         </div>
         <div className="cards">
           <div className="card">
             <p className="percetual">60%</p>
+            <p className="tecnologia">Javascript</p>
           </div>
           <div className="card">
             <p className="percetual">60%</p>
+            <p className="tecnologia">Javascript</p>
           </div>
           <div className="card">
             <p className="percetual">60%</p>
+            <p className="tecnologia">Javascript</p>
           </div>
           <div className="card">
             <p className="percetual">60%</p>
+            <p className="tecnologia">Javascript</p>
           </div>
           <div className="card">
             <p className="percetual">60%</p>
+            <p className="tecnologia">Javascript</p>
           </div>
           <div className="card">
             <p className="percetual">60%</p>
+            <p className="tecnologia">Javascript</p>
           </div>
           <div className="card">
             <p className="percetual">60%</p>
+            <p className="tecnologia">Javascript</p>
           </div>
           <div className="card">
             <p className="percetual">60%</p>
+            <p className="tecnologia">Javascript</p>
           </div>
         </div>
 
